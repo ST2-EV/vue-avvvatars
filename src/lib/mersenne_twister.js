152a5c765b0ec0be52bda63d4f6efd76836816a8
ST2-EV <steve.s.paul22@gmail.com>
2023-02-15 22:10:50 -0800

    refactored mersenne_twister to use "class "

--- src/lib/mersenne_twister.js
+++ src/lib/mersenne_twister.js
@@ -41,128 +41,130 @@ Any feedback is very welcome.
 http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
 email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
 */
-const MersenneTwister = function (self, seed) {
-  if (seed === undefined) {
-    // kept random number same size as time used previously to ensure no unexpected results downstream
-    seed = Math.floor(Math.random() * Math.pow(10, 13));
+
+class MersenneTwister {
+  constructor(seed) {
+    if (seed === undefined) {
+      // kept random number same size as time used previously to ensure no unexpected results downstream
+      seed = Math.floor(Math.random() * Math.pow(10, 13));
+    }
+    /* Period parameters */
+    this.N = 624;
+    this.M = 397;
+    this.MATRIX_A = 0x9908b0df;   /* constant vector a */
+    this.UPPER_MASK = 0x80000000; /* most significant w-r bits */
+    this.LOWER_MASK = 0x7fffffff; /* least significant r bits */
+    this.mt = new Array(this.N); /* the array for the state vector */
+    this.mti = this.N + 1; /* mti==N + 1 means mt[N] is not initialized */
+    this.init_genrand(seed);
   }
-  /* Period parameters */
-  self.N = 624;
-  self.M = 397;
-  self.MATRIX_A = 0x9908b0df;   /* constant vector a */
-  self.UPPER_MASK = 0x80000000; /* most significant w-r bits */
-  self.LOWER_MASK = 0x7fffffff; /* least significant r bits */
-  self.mt = new Array(self.N); /* the array for the state vector */
-  self.mti = self.N + 1; /* mti==N + 1 means mt[N] is not initialized */
-  self.init_genrand(seed);
+
+  init_genrand(s) {
+    this.mt[0] = s >>> 0;
+    for (this.mti = 1; this.mti < this.N; this.mti++) {
+      s = this.mt[this.mti - 1] ^ (this.mt[this.mti - 1] >>> 30);
+      this.mt[this.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253) + this.mti;
+      /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+      /* In the previous versions, MSBs of the seed affect   */
+      /* only MSBs of the array mt[].                        */
+      /* 2002/01/09 modified by Makoto Matsumoto             */
+      this.mt[this.mti] >>>= 0;
+      /* for >32 bit machines */
+    }
+  }
+
+  /* initialize by an array with array-length */
+  /* init_key is the array for initializing keys */
+  /* key_length is its length */
+  /* slight change for C++, 2004/2/26 */
+  init_by_array(init_key, key_length) {
+    var i = 1, j = 0, k, s;
+    this.init_genrand(19650218);
+    k = (this.N > key_length ? this.N : key_length);
+    for (; k; k--) {
+      s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30);
+      this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525))) + init_key[j] + j; /* non linear */
+      this.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
+      i++;
+      j++;
+      if (i >= this.N) { this.mt[0] = this.mt[this.N - 1]; i = 1; }
+      if (j >= key_length) { j = 0; }
+    }
+    for (k = this.N - 1; k; k--) {
+      s = this.mt[i - 1] ^ (this.mt[i - 1] >>> 30);
+      this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941)) - i; /* non linear */
+      this.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
+      i++;
+      if (i >= this.N) { this.mt[0] = this.mt[this.N - 1]; i = 1; }
+    }
+
+    this.mt[0] = 0x80000000; /* MSB is 1; assuring non-zero initial array */
+  }
+
+  /* generates a random number on [0,0xffffffff]-interval */
+  genrand_int32() {
+    var y;
+    var mag01 = new Array(0x0, this.MATRIX_A);
+    /* mag01[x] = x * MATRIX_A  for x=0,1 */
+
+    if (this.mti >= this.N) { /* generate N words at one time */
+      var kk;
+
+      if (this.mti === this.N + 1) {   /* if init_genrand() has not been called, */
+        this.init_genrand(5489); /* a default initial seed is used */
+      }
+      for (kk = 0; kk < this.N - this.M; kk++) {
+        y = (this.mt[kk] & this.UPPER_MASK) | (this.mt[kk + 1] & this.LOWER_MASK);
+        this.mt[kk] = this.mt[kk + this.M] ^ (y >>> 1) ^ mag01[y & 0x1];
+      }
+      for (; kk < this.N - 1; kk++) {
+        y = (this.mt[kk] & this.UPPER_MASK) | (this.mt[kk + 1] & this.LOWER_MASK);
+        this.mt[kk] = this.mt[kk + (this.M - this.N)] ^ (y >>> 1) ^ mag01[y & 0x1];
+      }
+      y = (this.mt[this.N - 1] & this.UPPER_MASK) | (this.mt[0] & this.LOWER_MASK);
+      this.mt[this.N - 1] = this.mt[this.M - 1] ^ (y >>> 1) ^ mag01[y & 0x1];
+
+      this.mti = 0;
+    }
+
+    y = this.mt[this.mti++];
+
+    /* Tempering */
+    y ^= (y >>> 11);
+    y ^= (y << 7) & 0x9d2c5680;
+    y ^= (y << 15) & 0xefc60000;
+    y ^= (y >>> 18);
+    return y >>> 0;
+  }
+
+  /* generates a random number on [0,0x7fffffff]-interval */
+  genrand_int31() {
+    return (this.genrand_int32() >>> 1);
+  };
+
+  /* generates a random number on [0,1]-real-interval */
+  genrand_real1() {
+    return this.genrand_int32() * (1.0 / 4294967295.0);
+    /* divided by 2^32-1 */
+  };
+
+  /* generates a random number on [0,1)-real-interval */
+  random() {
+    return this.genrand_int32() * (1.0 / 4294967296.0);
+    /* divided by 2^32 */
+  };
+
+  /* generates a random number on (0,1)-real-interval */
+  genrand_real3() {
+    return (this.genrand_int32() + 0.5) * (1.0 / 4294967296.0);
+    /* divided by 2^32 */
+  };
+
+  /* generates a random number on [0,1) with 53-bit resolution*/
+  genrand_res53() {
+    var a = this.genrand_int32() >>> 5, b = this.genrand_int32() >>> 6;
+    return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0);
+  };
 }
-
-/* initializes mt[N] with a seed */
-MersenneTwister.prototype.init_genrand = function (s) {
-  self.mt[0] = s >>> 0;
-  for (self.mti = 1; self.mti < self.N; self.mti++) {
-    s = self.mt[self.mti - 1] ^ (self.mt[self.mti - 1] >>> 30);
-    self.mt[self.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253) + self.mti;
-    /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-    /* In the previous versions, MSBs of the seed affect   */
-    /* only MSBs of the array mt[].                        */
-    /* 2002/01/09 modified by Makoto Matsumoto             */
-    self.mt[self.mti] >>>= 0;
-    /* for >32 bit machines */
-  }
-};
-
-/* initialize by an array with array-length */
-/* init_key is the array for initializing keys */
-/* key_length is its length */
-/* slight change for C++, 2004/2/26 */
-MersenneTwister.prototype.init_by_array = function (init_key, key_length) {
-  var i = 1, j = 0, k, s;
-  self.init_genrand(19650218);
-  k = (self.N > key_length ? self.N : key_length);
-  for (; k; k--) {
-    s = self.mt[i - 1] ^ (self.mt[i - 1] >>> 30);
-    self.mt[i] = (self.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525))) + init_key[j] + j; /* non linear */
-    self.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
-    i++;
-    j++;
-    if (i >= self.N) { self.mt[0] = self.mt[self.N - 1]; i = 1; }
-    if (j >= key_length) { j = 0; }
-  }
-  for (k = self.N - 1; k; k--) {
-    s = self.mt[i - 1] ^ (self.mt[i - 1] >>> 30);
-    self.mt[i] = (self.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941)) - i; /* non linear */
-    self.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
-    i++;
-    if (i >= self.N) { self.mt[0] = self.mt[self.N - 1]; i = 1; }
-  }
-
-  self.mt[0] = 0x80000000; /* MSB is 1; assuring non-zero initial array */
-};
-
-/* generates a random number on [0,0xffffffff]-interval */
-MersenneTwister.prototype.genrand_int32 = function () {
-  var y;
-  var mag01 = new Array(0x0, self.MATRIX_A);
-  /* mag01[x] = x * MATRIX_A  for x=0,1 */
-
-  if (self.mti >= self.N) { /* generate N words at one time */
-    var kk;
-
-    if (self.mti === self.N + 1) {   /* if init_genrand() has not been called, */
-      self.init_genrand(5489); /* a default initial seed is used */
-    }
-    for (kk = 0; kk < self.N - self.M; kk++) {
-      y = (self.mt[kk] & self.UPPER_MASK) | (self.mt[kk + 1] & self.LOWER_MASK);
-      self.mt[kk] = self.mt[kk + self.M] ^ (y >>> 1) ^ mag01[y & 0x1];
-    }
-    for (; kk < self.N - 1; kk++) {
-      y = (self.mt[kk] & self.UPPER_MASK) | (self.mt[kk + 1] & self.LOWER_MASK);
-      self.mt[kk] = self.mt[kk + (self.M - self.N)] ^ (y >>> 1) ^ mag01[y & 0x1];
-    }
-    y = (self.mt[self.N - 1] & self.UPPER_MASK) | (self.mt[0] & self.LOWER_MASK);
-    self.mt[self.N - 1] = self.mt[self.M - 1] ^ (y >>> 1) ^ mag01[y & 0x1];
-
-    self.mti = 0;
-  }
-
-  y = self.mt[self.mti++];
-
-  /* Tempering */
-  y ^= (y >>> 11);
-  y ^= (y << 7) & 0x9d2c5680;
-  y ^= (y << 15) & 0xefc60000;
-  y ^= (y >>> 18);
-  return y >>> 0;
-};
-
-/* generates a random number on [0,0x7fffffff]-interval */
-MersenneTwister.prototype.genrand_int31 = function () {
-  return (self.genrand_int32() >>> 1);
-};
-
-/* generates a random number on [0,1]-real-interval */
-MersenneTwister.prototype.genrand_real1 = function () {
-  return self.genrand_int32() * (1.0 / 4294967295.0);
-  /* divided by 2^32-1 */
-};
-
-/* generates a random number on [0,1)-real-interval */
-MersenneTwister.prototype.random = function () {
-  return self.genrand_int32() * (1.0 / 4294967296.0);
-  /* divided by 2^32 */
-};
-
-/* generates a random number on (0,1)-real-interval */
-MersenneTwister.prototype.genrand_real3 = function () {
-  return (self.genrand_int32() + 0.5) * (1.0 / 4294967296.0);
-  /* divided by 2^32 */
-};
-
-/* generates a random number on [0,1) with 53-bit resolution*/
-MersenneTwister.prototype.genrand_res53 = function () {
-  var a = self.genrand_int32() >>> 5, b = self.genrand_int32() >>> 6;
-  return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0);
-};
 
 export default MersenneTwister
